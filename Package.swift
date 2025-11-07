d793eb355ac37956902ff4b1f33d033928410fe0
Robert Hölzl <robert.hoelzl@baltech.de>
2025-11-07 15:54:57 +0100

    [internal] Update Package.swift to use GitHub release asset URL for SPM

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "MobileIDSdk",
-            url: "https://github.com/baltech-ag/MobileIDSDK-pkg/raw/master/sdk/ios/v0.10.00/MobileIDSDK.xcframework.zip",
+            url: "https://github.com/baltech-ag/MobileIDSDK-pkg/releases/download/0.10.0/MobileIDSDK.xcframework.zip",
             checksum: "34a9a92f8b08c1d2144d2b11049877d4cf6712129e8a5369e02394b45fbc53c8")
     ]
 )
