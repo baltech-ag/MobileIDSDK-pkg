f802c153814c73856155fcc43972396aa74c9a42
Robert Hölzl <robert.hoelzl@baltech.de>
2025-11-18 15:00:23 +0100

    Fix version format to SemVer (0.14.0)

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "MobileIDSdk",
-            url: "https://github.com/baltech-ag/MobileIDSDK-pkg/raw/master/sdk/ios/v0.14.00/MobileIDSDK.xcframework.zip",
+            url: "https://github.com/baltech-ag/MobileIDSDK-pkg/raw/master/sdk/ios/v0.14.0/MobileIDSDK.xcframework.zip",
             checksum: "dff45f8caee90b59aa4fd991025497f8273ea0a60008224816a148d7307cea9c")
     ]
 )
